4ec70a73bdbbb09a0e9bbf5323dfcb30d6aa2e05
Richard Oliveira <richardlemos@unigranrio.br>
2018-05-08 20:07:10 -0300

    Exemplo REST aula

---
+++ src/rest/Exemplo1.java
@@ -0,0 +1,16 @@
+package rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+@Path("/echo")
+public class Exemplo1 {
+	
+	@GET
+	@Produces("text/plain")
+	public String Echo(@QueryParam("word") String palavra) {
+		return "echo: " + palavra;
+	}
+}
